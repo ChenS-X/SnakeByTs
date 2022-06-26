c3d6daefa8c38b13907f068afe4a198a62f75bd2
ChenS-X <chensxyouxiang@163.com>
2022-06-26 15:45:58 +0800

    分包css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const proPlugins = [];
+if (isProd) {
+    proPlugins.push(new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+    }))
+}
 
 module.exports = {
     entry: './src/index.ts',
@@ -12,15 +25,14 @@ module.exports = {
         extensions: [".js", ".ts"]
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
             },
             {
                 test: /\.s[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -28,6 +40,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             inject: false
-        })
+        }),
+        new CleanWebpackPlugin(),
+        ...proPlugins
     ]
 }
